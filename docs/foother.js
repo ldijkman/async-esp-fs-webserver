c2ecefa78461d1d228ffacc78c265ebed110bb49
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2024-02-19 06:23:42 +0100

    Update foother.js

--- docs/foother.js
+++ docs/foother.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
     floatingDiv.style.cssText = 'position: fixed; bottom: 0; left: 0; width: 100%; background-color: #333; color: white; text-align: center; padding: 10px 0;';
 
     const links = [
-        {href: 'https://github.com/ldijkman/async-esp-fs-webserver/tree/master/docs', color: 'blue', text: 'Github DOCS async-esp-fs-webserver'},
-        {href: 'https://github.com/ldijkman/async-esp-fs-webserver/issues', color: 'yellowgreen', text: 'GitHub Issues'},
-        {href: 'https://github.com/ldijkman/async-esp-fs-webserver/discussions', color: 'yellowgreen', text: 'GitHub Discussions'},
+        {href: 'https://github.com/ldijkman/async-esp-fs-webserver/tree/master/docs', color: 'blue', text: 'Github DOCS Source'},
+        {href: 'https://github.com/ldijkman/async-esp-fs-webserver/issues', color: 'yellowgreen', text: 'Issues'},
+        {href: 'https://github.com/ldijkman/async-esp-fs-webserver/discussions', color: 'yellowgreen', text: 'Discussions'},
         {href: 'https://ldijkman.github.io/async-esp-fs-webserver/WebSerialMonitor.html', color: 'lightblue', text: 'Serial Monitor'},
-        {href: 'https://ldijkman.github.io/async-esp-fs-webserver/', color: 'red', text: 'Flash 4mb ESP8266 / ESP32 in browser'}
+        {href: 'https://ldijkman.github.io/async-esp-fs-webserver/', color: 'red', text: 'Flash 4mb ESP inBrowser'}
     ];
 
     links.forEach(link => {
