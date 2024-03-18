bb83db5704e2798f14a1ee660971f775ae659fde
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2024-03-18 16:52:48 +0100

    Update ESP32.ino

--- docs/ino/ESP32.ino
+++ docs/ino/ESP32.ino
@@ -315,7 +315,7 @@ void setup() {
   //   request->send(LittleFS, "/index.html", "text/html");
   // });
 
-  // Default headers for all responses
+    // Default headers for all responses
   DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
 
   /*
@@ -354,73 +354,126 @@ void setup() {
 
 
   server.on("/bulb.html", HTTP_GET, [](AsyncWebServerRequest * request) {
-    if (request->url() != "/edit") {
-      String path = "/bulb.html"; // Directly specify the file path
-      String contentType = "text/html"; // We know it's an HTML file
+    if (request->hasHeader("Referer")) {
+      AsyncWebHeader* refererHeader = request->getHeader("Referer");
+      String refererValue = refererHeader->value();
+      Serial.print("Referer: ");
+      Serial.println(refererValue);
 
-      File file = LittleFS.open(path, "r");
-      if (!file) {
-        request->send(404, "text/html", "Oh No, Not found <a href=\"/\">/ home</a>");
-        return;
+      // Check if the Referer does not contain "/ace"
+      if (refererValue.indexOf("/ace") != -1 || refererValue.indexOf("/edit") != -1) {
+        if (LittleFS.exists("/bulb.html")) {
+          Serial.println("bulb.html exists. Serving it now without processing for editor...");
+          request->send(LittleFS, "/bulb.html", "text/html");
+        }
+      } else {
+
+
+        String path = "/bulb.html"; // Directly specify the file path
+        String contentType = "text/html"; // We know it's an HTML file
+
+        File file = LittleFS.open(path, "r");
+        if (!file) {
+          request->send(404, "text/html", "Oh No, Not found <a href=\"/\">/ home</a>");
+          return;
+        }
+
+        String fileContent = file.readString();
+        file.close();
+
+        // Perform dynamic content replacement
+        fileContent.replace("%STATE%", ledState ? "ON" : "OFF");
+        fileContent.replace("%MDNS%", String(myhostname) + ".local");
+        fileContent.replace("%IP%", WiFi.localIP().toString());
+
+        request->send(200, contentType, fileContent);
       }
-
-      String fileContent = file.readString();
-      file.close();
-
-      // Perform dynamic content replacement
-      fileContent.replace("%STATE%", ledState ? "ON" : "OFF");
-      fileContent.replace("%MDNS%", String(myhostname) + ".local");
-      fileContent.replace("%IP%", WiFi.localIP().toString());
-
-      request->send(200, contentType, fileContent);
     }
   });
+
+
+
+
 
 
   server.on("/bulbs.html", HTTP_GET, [](AsyncWebServerRequest * request) {
-    if (request->url() != "/ace") {
-      String path = "/bulbs.html"; // Directly specify the file path
-      String contentType = "text/html"; // We know it's an HTML file
+    if (request->hasHeader("Referer")) {
+      AsyncWebHeader* refererHeader = request->getHeader("Referer");
+      String refererValue = refererHeader->value();
+      Serial.print("Referer: ");
+      Serial.println(refererValue);
 
-      File file = LittleFS.open(path, "r");
-      if (!file) {
-        request->send(404, "text/html", "Oh No, Not found <a href=\"/\">/ home</a>");
-        return;
+      // Check if the Referer does not contain "/ace"
+      if (refererValue.indexOf("/ace") != -1 || refererValue.indexOf("/edit") != -1) {
+        if (LittleFS.exists("/bulbs.html")) {
+          Serial.println("bulbs.html exists. Serving it now without processing for editor...");
+          request->send(LittleFS, "/bulbs.html", "text/html");
+        }
+      } else {
+        String path = "/bulbs.html"; // Directly specify the file path
+        String contentType = "text/html"; // We know it's an HTML file
+
+        File file = LittleFS.open(path, "r");
+        if (!file) {
+          request->send(404, "text/html", "Oh No, Not found <a href=\"/\">/ home</a>");
+          return;
+        }
+
+        String fileContent = file.readString();
+        file.close();
+
+        // Perform dynamic content replacement
+        fileContent.replace("%STATE%", ledState ? "ON" : "OFF");
+        fileContent.replace("%MDNS%", String(myhostname) + ".local");
+        fileContent.replace("%IP%", WiFi.localIP().toString());
+
+        request->send(200, contentType, fileContent);
       }
-
-      String fileContent = file.readString();
-      file.close();
-
-      // Perform dynamic content replacement
-      fileContent.replace("%STATE%", ledState ? "ON" : "OFF");
-      fileContent.replace("%MDNS%", String(myhostname) + ".local");
-      fileContent.replace("%IP%", WiFi.localIP().toString());
-
-      request->send(200, contentType, fileContent);
     }
+
   });
 
 
+
+
+
+
+
+
   server.on("/nerd.html", HTTP_GET, [](AsyncWebServerRequest * request) {
-    if (request->url() != "/ace") {
-      String path = "/nerd.html"; // Directly specify the file path
-      String contentType = "text/html"; // We know it's an HTML file
+    if (request->hasHeader("Referer")) {
+      AsyncWebHeader* refererHeader = request->getHeader("Referer");
+      String refererValue = refererHeader->value();
+      Serial.print("Referer: ");
+      Serial.println(refererValue);
 
-      File file = LittleFS.open(path, "r");
-      if (!file) {
-        request->send(404, "text/html", "Oh No, Not found <a href=\"/\">/ home</a>");
-        return;
+      // Check if the Referer does not contain "/ace"
+      if (refererValue.indexOf("/ace") != -1 || refererValue.indexOf("/edit") != -1) {
+        if (LittleFS.exists("/nerd.html")) {
+          Serial.println("nerd.html exists. Serving it now without processing for editor...");
+          request->send(LittleFS, "/nerd.html", "text/html");
+        }
+
+      } else {
+        String path = "/nerd.html"; // Directly specify the file path
+        String contentType = "text/html"; // We know it's an HTML file
+
+        File file = LittleFS.open(path, "r");
+        if (!file) {
+          request->send(404, "text/html", "Oh No, Not found <a href=\"/\">/ home</a>");
+          return;
+        }
+
+        String fileContent = file.readString();
+        file.close();
+
+        // Perform dynamic content replacement
+        fileContent.replace("%STATE%", ledState ? "ON" : "OFF");
+        fileContent.replace("%MDNS%", String(myhostname) + ".local");
+        fileContent.replace("%IP%", WiFi.localIP().toString());
+
+        request->send(200, contentType, fileContent);
       }
-
-      String fileContent = file.readString();
-      file.close();
-
-      // Perform dynamic content replacement
-      fileContent.replace("%STATE%", ledState ? "ON" : "OFF");
-      fileContent.replace("%MDNS%", String(myhostname) + ".local");
-      fileContent.replace("%IP%", WiFi.localIP().toString());
-
-      request->send(200, contentType, fileContent);
     }
   });
 
