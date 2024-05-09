92243ad2501d5da43b9c4a04296fbf191b284682
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2024-05-09 04:32:35 +0200

    Update chatgpt_thermostat.ino

--- docs/ino/thermostat_web_flash/chatgpt_thermostat.ino
+++ docs/ino/thermostat_web_flash/chatgpt_thermostat.ino
@@ -570,9 +570,18 @@ bot.sendMessage(CHAT_ID, message.c_str(), "");
         // "The Text" property is what shows up in the keyboard
         // The "callback_data" property is the text that gets sent when pressed  
         
-        String keyboardJson = F("[[{ \"text\" : \"ON\", \"callback_data\" : \"ON\" },{ \"text\" : \"OFF\", \"callback_data\" : \"OFF\" }],");
-        keyboardJson += F("[{ \"text\" : \"10 Mins\", \"callback_data\" : \"TIME10\" }, { \"text\" : \"20 Mins\", \"callback_data\" : \"TIME20\" }, { \"text\" : \"30 Mins\", \"callback_data\" : \"TIME30\" }]]");
-        bot.sendMessageWithInlineKeyboard(chat_id, "Sadbhs Stars", "", keyboardJson);
+        String message = "Thermostat started \n http://" + String(mDNS_adress) + ".local\nhttp://" + WiFi.localIP().toString();
+bot.sendMessage(CHAT_ID, message.c_str(), "");
+String keyboardJson = F("[[{ \"text\" : \"ON\", \"callback_data\" : \"ON\" },{ \"text\" : \"OFF\", \"callback_data\" : \"OFF\" }],");
+keyboardJson += F("[{ \"text\" : \"10 Mins\", \"callback_data\" : \"TIME10\" }, { \"text\" : \"20 Mins\", \"callback_data\" : \"TIME20\" }, { \"text\" : \"30 Mins\", \"callback_data\" : \"TIME30\" }],");
+keyboardJson += F("[{ \"text\" : \"15 Celsius\", \"callback_data\" : \"TEMP15\" }, { \"text\" : \"20 Celsius\", \"callback_data\" : \"TEMP20\" }]]");
+bot.sendMessageWithInlineKeyboard(CHAT_ID, "Thermostat Control", "", keyboardJson);
+ 
+// Assuming temperatureSetpoint is a float
+// Assuming currentTemperature holds the current temperature
+message = "Setpoint: " + String(temperatureSetpoint, 1) + "°C, Current Temp: " + String(sensors.getTempCByIndex(0), 1) + "°C"; // 1 decimal place for float
+bot.sendMessage(CHAT_ID, message.c_str(), "");
+bot.sendMessage(CHAT_ID, "https://t.me/s/Luberth_Dijkman", "");
       }
 
       // When a user first uses a bot they will send a "/start" command
